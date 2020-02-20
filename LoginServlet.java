d316d2991a81d92d7bc9be3af24991441eb387ae
Devansh Devagya <42578725+Devansh2800@users.noreply.github.com>
2020-02-20 17:53:47 +0530

    Basic Program 2

---
+++ LoginServlet.java
@@ -0,0 +1,44 @@
+import javax.servlet.*;
+import java.io.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class LoginServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException
+	{
+	PrintWriter out = res.getWriter();
+	res.setContentType("text/html");
+	
+	
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost/login?user=root&password=");
+		PreparedStatement ps=con.prepareStatement("select * from mylogin where uid=? and password=?");
+		String a= req.getParameter("t1");
+		String b= req.getParameter("n1");
+		ps.setString(1,a);
+		ps.setString(2,b);
+		ResultSet rs=ps.executeQuery();
+		RequestDispatcher rd=null;
+		
+		if(rs.next()){
+			HttpSession ses=req.getSession();
+			
+			rd=req.getRequestDispatcher("welcome");
+			ses.setAttribute("user",a);
+			rd.forward(req,res);
+			
+			
+		}
+		else{
+			out.println("Error:Either username or password incorrect"+"\n");
+			out.println("Enter Again!!");
+			rd=req.getRequestDispatcher("Login.html");
+			rd.include(req,res);
+		}
+		}
+	catch(Exception e){
+		out.println(e);
+	}
+	}
+}
