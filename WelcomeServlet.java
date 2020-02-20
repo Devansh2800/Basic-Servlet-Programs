a7390cbb71e8afd598a09908cd1bb74f5a6352da
Devansh Devagya <42578725+Devansh2800@users.noreply.github.com>
2020-02-20 17:57:45 +0530

    Welcome page for loginservlet file.

---
+++ WelcomeServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.*;
+import java.io.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class WelcomeServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException
+	{
+	PrintWriter out = res.getWriter();
+	res.setContentType("text/html");
+	HttpSession ses=req.getSession(false);
+	if(ses==null)
+	{
+		res.sendRedirect("CheckLogin");
+	}
+	else
+	{
+		String uid=(String)ses.getAttribute("user");
+		out.println("Welcome "+uid);
+	}
+	}
+}
